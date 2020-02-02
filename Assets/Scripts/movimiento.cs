6bafc080fe8ec804ea306c73020e0e9592f40328
Ricardo Lázaro <caffeine305@gmail.com>
2020-02-01 23:52:40 -0600

    Added Map-Scenes!

--- Assets/Scripts/movimiento.cs
+++ Assets/Scripts/movimiento.cs
@@ -9,49 +9,32 @@ public class movimiento : MonoBehaviour
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
     public float thereshold = 0.0001f;
-    public float quacks = 0.5f;
-    public float deployTime = 0.0f;
 
     public Animator Animar;
-    public GameObject path;
 
     private Vector3 moveDirection = Vector3.zero;
-    private Vector3 thrshy = Vector3.zero;
-    private Vector3 playerPos = Vector3.zero;
-    private Vector3 transf2Vec3 = Vector3.zero;
-
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         Animar = gameObject.GetComponentInParent<Animator>();
-        //characterPosition = GetComponent<Trasform>();
-        deployTime = 0.0f;
+        
     }
 
     void Update()
     {
-    deployTime += Time.deltaTime;
-    thrshy = new Vector3(1.0f,1.0f,1.0f);
- 
         if (characterController.isGrounded)
         {
             // We are grounded, so recalculate
             // move direction directly from axes
 
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            transform.LookAt(transform.position+moveDirection, Vector3.up);
             moveDirection *= speed;
-            
-            playerPos = transform.localPosition;
 
-            if( moveDirection.magnitude > thereshold )
-                {
-                //if (deployTime > quacks)
-                //    {
-                        Instantiate(path, playerPos + thrshy , Quaternion.identity);
-                //        deployTime = 0.0f;
-                //    }
-                //Animar.SetBool("IsWalking", true);
-                }//else Animar.SetBool("IsWalking", false);
+            //if( moveDirection.magnitude > thereshold )
+            //    {
+            //    Animar.SetBool("IsWalking", true);
+            //    }else Animar.SetBool("IsWalking", false);
 
             /*
             if (Input.GetButton("Jump"))
@@ -71,7 +54,7 @@ public class movimiento : MonoBehaviour
         // as an acceleration (ms^-2)
         moveDirection.y -= gravity * Time.deltaTime;
 
-        // Mover charactercontroller
+        // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
     }
 }
